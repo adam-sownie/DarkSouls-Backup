592b9eea0516fa15d6c1b4e175f8b4a2468e25aa
Adam Sownie <sownieadam@gmail.com>
2016-04-29 20:36:14 -0700

    Create darksouls_backup.ps1
    committed backup script

---
+++ darksouls_backup.ps1
@@ -0,0 +1,21 @@
+$backupDir = (Join-Path $env:UserProfile Documents\dks_bk)
+$darkSoulsDir = (Join-Path $env:AppData "DarkSoulsIII")
+$backupNumber = 5
+
+if(!(Test-Path $backupDir)) {
+  New-Item $backupDir -Type Directory
+
+  for($i = $backupNumber; $i -gt 0; $i = $i - 1) {
+    New-Item (Join-Path $backupDir $i) -Type Directory
+  }
+}
+
+Remove-Item -Path (Join-Path $backupDir $backupNumber) -Recurse -Force
+
+for($i = $backupNumber; $i -gt 1; $i = $i - 1) {
+  Rename-Item -Path (Join-Path $backupDir ($i - 1)) -NewName (Join-Path $backupDir $i)
+}
+
+Copy-Item $darkSoulsDir (Join-Path $backupDir 1) -Recurse
+
+Write-Host ("DarkSouls save data was backed up to " + $backupDir) -ForeGroundColor "Green"
